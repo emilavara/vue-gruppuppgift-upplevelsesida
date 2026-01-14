8db34de5526901f0660f97dd5eba90c2412e8637
CreateWebGit <dev@createweb.se>
2026-01-14 10:30:38 +0000

    initial commit, file structure

---
+++ app/components/ui/FirstComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, computed, onMounted } from "vue";
+
+let count = ref<number>(0);
+
+const doubled = computed(() => count.value * 2)
+
+let text = ref<string>('hejsan')
+
+function increment() {
+	console.log(text)
+	count.value++ //måste unwrappas -> till value
+}
+
+onMounted(() => {
+	// alert('mountades')
+	document.getElementById('text').style.color = "red"
+})
+</script>
+
+<template>
+	<p id="text">{{ count }}</p>
+	doubled: {{doubled}}
+	<button @click="increment">inkrementera</button>
+	<input v-model="text" type="text"/>
+</template>
+
+<style></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>hej</h1>
+</template>
